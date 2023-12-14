804c532e379f1ed5996f8a0a078e48e8bb845078
wPatr <91604767+wPatr@users.noreply.github.com>
2023-12-14 11:43:23 +0100

    Refactor: sends List<Object> to view

--- Patjo-booking-system/src/java/controller/BookingController.java
+++ Patjo-booking-system/src/java/controller/BookingController.java
@@ -1,44 +1,52 @@
 package controller;
 
 import java.util.List;
+import model.CourseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import service.BookingService;
 
 /**
  *
  * BookingController class handles requests related to booking operations.
- * 
+ *
  */
-
 @Controller
 @RequestMapping("/booking")
 public class BookingController {
-    
+
     private final BookingService bookingService;
-    
-     @Autowired
+
+    @Autowired
     public BookingController(BookingService bookingService) {
         this.bookingService = bookingService;
     }
-    
+
     /**
-     * Handles GET request for displaying available courses.
-     * Retrieves a list with available courses from the BookingService class,
-     * and adds it to the model for rendering in view.
-     * 
+     * Handles GET request for displaying available courses. Retrieves a list
+     * with available courses from the BookingService class, and adds it to the
+     * model for rendering in view.
+     *
      * @param model
      * @return selectCourseView
      */
-    @GetMapping
-    public String showAvailableCourses(Model model){
-        
-        List<String> courseList = bookingService.fetchAvailableCourses();   
-        model.addAttribute("courseList",courseList);
-      
+   @GetMapping
+    public String showAvailableCourses(Model model) {
+
+        List<CourseDTO> courseList = bookingService.fetchAvailableCourses();
+        model.addAttribute("courseList", courseList);
+
         return "selectCourseView";
     }
+
+    /*@GetMapping("/timeslots")
+    public String showTimeSlotLists(@RequestParam("selectedCourse") int selectedCourseId, Model model) {
+        System.out.println("selected course id: " + selectedCourseId);
+        
+        return "selectedCourseView"; 
+    }*/
 }
