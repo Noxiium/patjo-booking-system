47d9758d7da210c3245b5e7c918a8f890892991d
wPatr <91604767+wPatr@users.noreply.github.com>
2023-12-14 10:36:11 +0100

    Create: Implement BookingService for booking-related business logic

--- Patjo-booking-system/src/java/service/BookingService.java
+++ Patjo-booking-system/src/java/service/BookingService.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package service;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.BookingRepository;
+
 /**
- *
- * @author patricialagerhult
+ * The BookingService class manages the business logic for handling booking-related operations.
+ * 
  */
+
+@Service
 public class BookingService {
     
+    private final BookingRepository bookingRepository;
+    
+    @Autowired
+    public BookingService(BookingRepository bookingRepository){
+        this.bookingRepository = bookingRepository;
+    }
+
+    /**
+    * Retrieves the list of available courses and returns it.
+    * 
+    * @return List of available courses
+     */
+    public List<String> fetchAvailableCourses() {
+        //List<String> courseList = bookingRepository.fetchAvailableCoursesFromDB();
+        
+        //Placeholder
+        List<String> courseList = new ArrayList<>();
+        courseList.add("ID1212");
+        courseList.add("ID2000");
+        
+        return courseList;
+    }
 }
