0c0170eb1268d24b94e6e5d89fa3b89bce9f3e3d
wPatr <91604767+wPatr@users.noreply.github.com>
2023-12-14 10:33:04 +0100

    Feature: Added BookingController class for handling requests related to booking operations.

--- Patjo-booking-system/src/java/controller/BookingController.java
+++ Patjo-booking-system/src/java/controller/BookingController.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import service.BookingService;
 
 /**
  *
- * @author patricialagerhult
+ * BookingController class handles requests related to booking operations.
+ * 
  */
+
+@Controller
+@RequestMapping("/booking")
 public class BookingController {
     
+    private final BookingService bookingService;
+    
+     @Autowired
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+    
+    /**
+     * Handles GET request for displaying available courses.
+     * Retrieves a list with available courses from the BookingService class,
+     * and adds it to the model for rendering in view.
+     * 
+     * @param model
+     * @return selectCourseView
+     */
+    @GetMapping
+    public String showAvailableCourses(Model model){
+        
+        List<String> courseList = bookingService.fetchAvailableCourses();   
+        model.addAttribute("courseList",courseList);
+      
+        return "selectCourseView";
+    }
 }
