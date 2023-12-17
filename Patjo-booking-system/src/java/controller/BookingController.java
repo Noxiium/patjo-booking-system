d112b1fec69259fcbf015d0d032245bc78046de2
wPatr <91604767+wPatr@users.noreply.github.com>
2023-12-17 16:53:03 +0100

    Added: Method for saving a booked time slot

--- Patjo-booking-system/src/java/controller/BookingController.java
+++ Patjo-booking-system/src/java/controller/BookingController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.util.List;
+import javax.servlet.http.HttpSession;
 import model.BookingDTO;
 import model.CourseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,7 +35,7 @@ public class BookingController {
      * @param model
      * @return selectCourseView
      */
-     @RequestMapping("/booking/showcourses")
+    @RequestMapping("/booking/showcourses")
     public String showAvailableCourses(Model model) {
         List<CourseDTO> courseList = bookingService.fetchAvailableCourses();
         model.addAttribute("courseList", courseList);
@@ -44,9 +45,10 @@ public class BookingController {
 
     /**
      *
-     * Handles GET request for displaying available time slots for the selected course.
-     * Retrieves a booking list with time slots from the BookingService class, and adds it to the
-     * model for rendering in view.
+     * Handles GET request for displaying available time slots for the selected
+     * course. Retrieves a booking list with time slots from the BookingService
+     * class, and adds it to the model for rendering in view.
+     *
      * @param selectedCourseId The id of the selected course
      * @param model
      * @return timeSlotsView
@@ -55,10 +57,26 @@ public class BookingController {
     public String showBookingListForCourse(@RequestParam("selectedCourse") int selectedCourseId, Model model) {
         System.out.println("selected course id: " + selectedCourseId);
         List<BookingDTO> bookingList = bookingService.fetchBookingListForCourse(selectedCourseId);
-       
-        model.addAttribute("bookingList", bookingList);
-        return "timeSlotsView"; 
-    }
-    
 
+        model.addAttribute("bookingList", bookingList);
+        return "timeSlotsView";
+    }
+
+    /**
+     * Handles the request to book an available time slot, saves the booking information in
+     * the database, and returns the view for confirming the booked time slot.
+     *
+     * @param selectedTimeSlot The selected time slot to be booked.
+     * @param session The HttpSession containing user information.
+     * @return The view name for confirming the booked time slot.
+     */
+    @RequestMapping("/booking/book-time-slot")
+    public String confirmBookedTime(@RequestParam("selectedTimeSlot") String selectedTimeSlot, HttpSession session) {
+
+        bookingService.saveBookedTime(selectedTimeSlot, session);
+
+        System.out.println("SelectedTimeSlot = " + selectedTimeSlot);
+
+        return "confirmedTimeSlotView";
+    }
 }
