3f0298e665d55e84500529f243085e067792b82c
wPatr <91604767+wPatr@users.noreply.github.com>
2024-01-01 18:08:03 +0100

    Generate presentation list functionality

--- Patjo-booking-system/src/java/service/PresentationListService.java
+++ Patjo-booking-system/src/java/service/PresentationListService.java
@@ -1,7 +1,10 @@
 package service;
 
 import controller.WebSocketEndpoint;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import model.BookingDTO;
@@ -58,9 +61,9 @@ public class PresentationListService {
     public void deleteSelectedPresentationList(int selectedListId) {
         presentationListRepository.deleteAssociatedBookings(selectedListId);
         presentationListRepository.deletePresentationListFromDB(selectedListId);
-        
+
         // Send a message to all connected WebSocket clients to notify them of an update.
-         WebSocketEndpoint.sendMessageToAll("updateBooking");
+        WebSocketEndpoint.sendMessageToAll("updateBooking");
     }
 
     /**
@@ -77,21 +80,32 @@ public class PresentationListService {
      * Saves the created presentation list along with associated bookings in the
      * database.
      *
-     * @param bookingDTOList List of BookingDTO objects representing the created
-     * Presentation List.
+     * @param typeOfSession
+     * @param location
+     * @param startTime
      * @param session HttpSession object containing user-related information.
      * @param courseId Integer representing the ID of the course for which the
      * presentation list is created.
      */
-    public void saveCreatedPresentationList(List<BookingDTO> bookingDTOList, HttpSession session, Integer courseId) {
+    public void saveCreatedPresentationList(List<String> typeOfSession, List<String> location, List<Date> startTime, HttpSession session, Integer courseId) {
+
+        // Get the ID of the user creating the list
         Integer userId = (Integer) session.getAttribute("userId");
 
+        // Build a list of BookingDTO objects 
+        List<BookingDTO> bookingDTOList = buildBookingDTOList(typeOfSession, location, startTime);
+
+        // Insert the presentation list and its creator into the database, returning the generated list ID
         int listId = presentationListRepository.saveListInDBReturnListId(userId, courseId);
+
+        // Save the associated bookings in the database, returning the generated booking IDs
         List<Integer> bookingIds = saveAssociatedBookings(bookingDTOList);
+
+        // Update the cross-reference table between the presentation list and bookings
         presentationListRepository.updateBookingListReferenceTable(listId, bookingIds);
-        
+
         // Send a message to all connected WebSocket clients to notify them of an update.
-         WebSocketEndpoint.sendMessageToAll("updateBooking");
+        WebSocketEndpoint.sendMessageToAll("updateBooking");
     }
 
     /**
@@ -111,5 +125,131 @@ public class PresentationListService {
         }
         return bookingIds;
 
+    }
+
+    /**
+     * Saves a generated presentation list in the database
+     *
+     * @param typeOfSession The type of session
+     * @param location The location
+     * @param startTime The starting time for the first time slot
+     * @param numberOfTimeSlots The total number of time slots in the
+     * presentation list.
+     * @param intervalBetweenTimeSlots The time interval between each time slot.
+     * @param courseId The ID of the course
+     * @param session The HttpSession containing information about the current
+     * user.
+     */
+    public void saveGeneratedPresentationList(String typeOfSession, String location,
+            Date startTime, int numberOfTimeSlots, int intervalBetweenTimeSlots,
+            String courseId, HttpSession session) {
+
+        // Get the ID of the user creating the list
+        Integer userId = (Integer) session.getAttribute("userId");
+
+        // Insert the presentation list and its creator into the database, returning the generated list ID
+        int listId = presentationListRepository.saveListInDBReturnListId(userId, Integer.valueOf(courseId));
+
+        // Calculate time intervals for all time slots in the presentation list
+        List<String> timeSlotIntervals = calculateTimeSlotIntervals(startTime, intervalBetweenTimeSlots, numberOfTimeSlots);
+
+        // Build a list of BookingDTO objects 
+        List<BookingDTO> bookingDTOList = buildBookingDTOListForGeneratedSlots(typeOfSession, location, timeSlotIntervals);
+
+        // Save the associated bookings in the database, returning the generated booking IDs
+        List<Integer> bookingIds = saveAssociatedBookings(bookingDTOList);
+
+        // Update the cross-reference table between the presentation list and bookings
+        presentationListRepository.updateBookingListReferenceTable(listId, bookingIds);
+
+        // Send a message to all connected WebSocket clients to notify them of an update.
+        WebSocketEndpoint.sendMessageToAll("updateBooking");
+    }
+
+    /**
+     * Builds a list of BookingDTO objects based on input lists for
+     * typeOfSession, location, and startTime.
+     *
+     * @param typeOfSession List of strings representing the types of sessions.
+     * @param location List of strings representing the locations associated
+     * with each session.
+     * @param startTime List of strings representing the start times for each
+     * session.
+     * @return A List of BookingDTO objects representing the booking information
+     * for each session.
+     */
+    private List<BookingDTO> buildBookingDTOList(List<String> typeOfSession, List<String> location, List<Date> startTime) {
+
+        List<BookingDTO> bookingDTOList = new ArrayList<>();
+
+        for (int i = 0; i < typeOfSession.size(); i++) {
+            BookingDTO bookingDTO = new BookingDTO();
+            bookingDTO.setTypeOfSession(typeOfSession.get(i));
+            bookingDTO.setLocation(location.get(i));
+            bookingDTO.setStartTime(formatDate(startTime.get(i)));
+            bookingDTO.setIsAvailable(true);
+
+            bookingDTOList.add(bookingDTO);
+        }
+        return bookingDTOList;
+    }
+
+    /**
+     * Creates time slot intervals based on the provided start time, interval
+     * between time slots, and the number of time slots.
+     *
+     * @param startTime The starting time for the first time slots.
+     * @param intervalBetweenTimeSlots The interval between time slots in
+     * minutes.
+     * @param numberOfTimeSlots The number of time slots to generate.
+     * @return A list of formatted time slot intervals.
+     */
+    private List<String> calculateTimeSlotIntervals(Date startTime, int intervalBetweenTimeSlots, int numberOfTimeSlots) {
+        List<String> timeSlots = new ArrayList<>();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(startTime);
+
+        for (int i = 0; i < numberOfTimeSlots; i++) {
+            timeSlots.add(formatDate(calendar.getTime()));
+            calendar.add(Calendar.MINUTE, intervalBetweenTimeSlots);
+        }
+        return timeSlots;
+    }
+
+    /**
+     * Formats the given date into a string with the pattern "yyyy-MM-dd HH:mm".
+     *
+     * @param date The date to be formatted.
+     * @return A formatted string representation of the date.
+     */
+    private String formatDate(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        return simpleDateFormat.format(date);
+    }
+
+    /**
+     * Builds a list of BookingDTO objects for the provided time slot intervals,
+     * type of session, and location.
+     *
+     * @param typeOfSession The type of session for the bookings.
+     * @param location The location for the bookings.
+     * @param timeSlotIntervals The list of formatted time slot intervals.
+     * @return A list of BookingDTO objects
+     */
+    private List<BookingDTO> buildBookingDTOListForGeneratedSlots(String typeOfSession, String location, List<String> timeSlotIntervals) {
+
+        List<BookingDTO> bookingDTOList = new ArrayList<>();
+
+        for (int i = 0; i < timeSlotIntervals.size(); i++) {
+            BookingDTO bookingDTO = new BookingDTO();
+            bookingDTO.setTypeOfSession(typeOfSession);
+            bookingDTO.setLocation(location);
+            bookingDTO.setStartTime(timeSlotIntervals.get(i));
+            bookingDTO.setIsAvailable(true);
+
+            bookingDTOList.add(bookingDTO);
+        }
+        return bookingDTOList;
     }
 }
